57b536a0969f9955632b54a8cafdcfbbeacd73c8
Mashal Pervaiz <mashalbutt1028@gmail.com>
2024-08-16 19:03:20 +0500

    sign in

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,33 @@
+import { AppBar, Toolbar, Typography, Button, Box, Container } from "@mui/material";  // Import Container correctly
+import Link from 'next/link';
+import { SignIn } from '@clerk/nextjs';
+
+export default function SignUpPage() {
+  return (
+    <Container maxWidth="sm">
+      <AppBar position="static" sx={{ backgroundColor: "#3f51b5" }}>
+        <Toolbar>
+          <Typography variant='h6' sx={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
+          <Button color="inherit">
+            <Link href="/sign-in" passHref>
+              Login
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link href="/sign-up" passHref>
+              Sign Up
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+        <Typography variant="h4">  {/* Corrected 'varirant' to 'variant' */}
+          Sign In
+        </Typography>
+        <SignIn />
+      </Box>
+    </Container>
+  );
+}
